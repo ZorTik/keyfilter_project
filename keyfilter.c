d470eaac99c2a2d54e7a0467a38fdbb7f13cd31b
ZorTik <zortlegit@gmail.com>
2023-11-16 17:42:07 +0100

    Update

--- keyfilter.c
+++ keyfilter.c
@@ -9,7 +9,7 @@
 // ,
 //#define DELIM 44
 // Max count
-#define MAX_C 42
+#define MAX_C 48
 #endif
 
 typedef struct {
@@ -18,75 +18,8 @@ typedef struct {
     int ends[MAX_C];
 } match_res;
 
-void read_db(char* str);
-void strupper(char* input);
-int strcontains(char* input, int c);
-match_res matchlocs(char* db, char* in);
-
 int strl(char* c) {
     return (int) strlen(c);
-}
-
-
-/**
- * Entrypoint.
- * ** argc
- * ** argv
-*/
-int main(int argc, char* argv[]) {
-    //char* input_db = "";
-    char* input_addr = "";
-    if (argc == 2) {
-        input_addr = argv[1];
-    }
-    char input_db[(100 * 43) * 8];
-    read_db(&input_db[0]);
-    if (input_db[0] == DELIM || input_db[strl(input_db) - 1] == DELIM) {
-        printf("Input can't start or end with %c!", DELIM);
-        return 0;
-    }
-    if (strcontains(input_addr, DELIM)) {
-        printf("Input can't contain %c", DELIM);
-        return 0;
-    }
-    // Convert the inputs to upper case (case insensitive)
-    strupper(input_db);
-    strupper(input_addr);
-    match_res m_res = matchlocs(input_db, input_addr);
-    // Match count
-    int m_count = m_res.amount;
-    // Match start indexes
-    int* starts = m_res.starts;
-    // Match end indexes
-    int* ends = m_res.ends;
-
-    if (m_count == 0) {
-        printf("Not found");
-    } else if (m_count == 1) {
-        printf("Found: ");
-        int i = starts[0];
-        while (i <= ends[0]) {
-            printf("%c", input_db[i++]);
-        }
-    } else {
-        printf("Enable: ");
-        int chars[m_count];
-        for (int i = 0; i < m_count; i++) {
-            chars[i] = input_db[
-                strl(input_addr) == 0 ? ends[i] : ends[i] + 1
-            ];
-            int duplicate = 0;
-            for (int i2 = 0; i2 < i; i2++) {
-                // Check for duplicates (Show only first occurencies)
-                if (chars[i2] == chars[i]) {
-                    duplicate = 1;
-                }
-            }
-            if (!duplicate) {
-                printf("%c", chars[i]);
-            }
-        }
-    }
 }
 
 void read_db(char* str) {
@@ -135,7 +68,7 @@ void put_word_starts(char* db, match_res* res) {
             loci++;
         }
     }
-    res->amount = loci + 1;
+    res->amount = loci;
 }
 
 void put_word_matches(char* db, char* in, match_res* res) {
@@ -210,7 +143,7 @@ void put_word_matches(char* db, char* in, match_res* res) {
  * 
  * returns: The match result
 */
-match_res matchlocs(char* db, char* in) {
+match_res match(char* db, char* in) {
     match_res res;
     if (strl(in) == 0) {
         put_word_starts(db, &res);
@@ -231,4 +164,83 @@ match_res matchlocs(char* db, char* in) {
         res.ends[0]++;
     }
     return res;
+}
+
+void sort_alphabetically(int chars[], int size) {
+    int i = 0;
+    while (i < size - 1) {
+        if (chars[i] > chars[i+1]) {
+            int temp = chars[i];
+            chars[i] = chars[i+1];
+            chars[i+1] = temp;
+            i = 0;
+            continue;
+        }
+        i++;
+    }
+}
+
+
+/**
+ * Entrypoint.
+ * ** argc
+ * ** argv
+*/
+int main(int argc, char* argv[]) {
+    //char* input_db = "";
+    char* input_addr = "";
+    if (argc == 2) {
+        input_addr = argv[1];
+    }
+    char input_db[(100 * 46) * 8];
+    read_db(&input_db[0]);
+    if (input_db[0] == DELIM || input_db[strl(input_db) - 1] == DELIM) {
+        printf("Input can't start or end with %c!", DELIM);
+        return 0;
+    }
+    // Convert the inputs to upper case (case insensitive)
+    strupper(input_db);
+    strupper(input_addr);
+    match_res m_res = match(input_db, input_addr);
+    // Match count
+    int m_count = m_res.amount;
+    // Match start indexes
+    int* starts = m_res.starts;
+    // Match end indexes
+    int* ends = m_res.ends;
+
+    if (m_count == 0) {
+        printf("Not found");
+    } else if (m_count == 1) {
+        printf("Found: ");
+        int i = starts[0];
+        while (i <= ends[0]) {
+            printf("%c", input_db[i++]);
+        }
+    } else {
+        printf("Enable: ");
+        int chars[m_count];
+        int c_i = 0;
+        for (int i = 0; i < m_count; i++) {
+            int c = input_db[
+                strl(input_addr) == 0 ? ends[i] : ends[i] + 1
+            ];
+            int duplicate = 0;
+            for (int i2 = 0; i2 < i; i2++) {
+                // Check for duplicates (Show only first occurencies)
+                if (chars[i2] == c) {
+                    duplicate = 1;
+                }
+            }
+            if (!duplicate) {
+                chars[c_i] = c;
+                c_i++;
+            }
+        }
+        sort_alphabetically(chars, c_i);
+        for (int i = 0; i < c_i; i++) {
+            printf("%c", chars[i]);
+        }
+    }
+    printf("\n");
 }
